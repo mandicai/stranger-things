8f60f5874da7085f711a29de9b96a895d3c6e94b
Mandi Cai <Mandi@Mandis-MacBook-Pro.local>
2016-12-06 15:53:00 -0500

    Added work so far on Stranger Things environment.

---
+++ Stranger Things_Real/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+
+	public GameObject light;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Random.value > 0.9 ) //a random chance
+		{
+			if ( light.activeInHierarchy ) //if the light is on...
+			{
+				light.SetActive(false); //turn it off
+			}
+			else
+			{
+				light.SetActive(true); //turn it on
+			}
+		}
+	}
+}
