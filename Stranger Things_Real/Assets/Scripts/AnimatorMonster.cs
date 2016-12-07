9b05232d34bb62b9f55e6a9439289efab8e482af
Mandi Cai <Mandi@Mandis-MacBook-Pro.local>
2016-12-06 23:32:06 -0500

    Deleted some unnecessary assets, added animation to the monster, added flickering lights.

---
+++ Stranger Things_Real/Assets/Scripts/AnimatorMonster.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorMonster : MonoBehaviour {
+
+	//Initialize variable 
+	public GameObject monster;
+	Animator animator;
+
+	void Start () {
+		// Grab the animator component 
+		animator = GetComponent<Animator> ();
+	}
+
+	void Update () {
+
+		// Detect if the space bar is down
+		// Grab the space bar input
+		// If the space bar is down, transition to the moving animation 
+		// Set the condition boolean to true 
+		// If the space bar is up, set the condition boolean to false
+
+		bool keydown = Input.GetKey("space"); // Detects if the spacebar is down
+		bool keyup = Input.GetKeyUp("space"); // Detects if the spacebar is up
+		bool attack = Input.GetKey("return"); // Detects if the enter key is down
+		bool spawn = Input.GetKey("s"); // Detects if the s key is down 
+
+		if (keydown) {
+			animator.SetBool ("WalkForward", true);
+			monster.transform.Translate (0, 0, 0.06f);
+		} else if (keyup) {
+			animator.SetBool ("WalkForward", false);
+		}
+
+		if (attack) {
+			animator.SetBool ("Attack", true);
+		} else {
+			animator.SetBool ("Attack", false);
+		}
+
+		if (spawn) {
+			animator.SetBool ("Spawn", true);
+		} else {
+			animator.SetBool ("Spawn", false);
+		}
+	}
+}
+			
---
+++ Stranger Things_Real/Assets/Scripts/LightFlicker2.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker2 : MonoBehaviour {
+
+	public Light directional_light;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Flicker() {
+		if ( Random.value > 0.9 ) //a random chance
+		{
+			if (directional_light.enabled) //if the light is on...
+			{
+				directional_light.enabled = false;; //turn it off
+			}
+			else
+			{
+				directional_light.enabled = true; //turn it on
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Flicker();
+	}
+}
